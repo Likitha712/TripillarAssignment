7ea35f86d7950a463c950beb02101cf2cab295a8
Likitha712 <likithab521@gmail.com>
2026-03-25 22:10:15 +0530

    Add files via upload

---
+++ Module10/LongestWord.java
@@ -0,0 +1,18 @@
+package com.TripilarCodingAssignment.Module10;
+class LongestWord {
+    public static void main(String[] args) {
+        String str = "Java programming is interesting";
+        String[] words = str.split(" ");
+
+        String longest = "";
+
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+
+        System.out.println("Longest word: " + longest);
+    }
+}
+
---
+++ Module10/ReplaceSpaces.java
@@ -0,0 +1,9 @@
+package com.TripilarCodingAssignment.Module10;
+class ReplaceSpaces {
+    public static void main(String[] args) {
+        String str = "Hello World Java";
+        str = str.replace(" ", "%20");
+
+        System.out.println(str);
+    }
+}
